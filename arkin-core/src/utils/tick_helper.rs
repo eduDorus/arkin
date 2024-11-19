2fd13766ad4a6b1da67eea5f9dbe4efa35adcd52
Dorus Janssens <dorus.janssens@gmail.com>
2024-11-19 23:50:57 +0800

    Add test-inegration and tick helper

--- arkin-core/src/utils/tick_helper.rs
+++ arkin-core/src/utils/tick_helper.rs
@@ -2,47 +2,64 @@ use time::{Duration, OffsetDateTime};
 use tokio::time::Duration as StdDuration;
 use tokio::time::Interval;
 use tokio::time::{interval_at, Instant, MissedTickBehavior};
+use tracing::debug;
 
-pub fn create_interval(interval: Duration) -> Interval {
-    let now = OffsetDateTime::now_utc();
-    println!("Now: {:?}", now);
+pub struct TickHelper {
+    pub interval: Interval,
+}
 
-    let epoch = OffsetDateTime::UNIX_EPOCH;
+impl TickHelper {
+    pub fn new(interval: Duration) -> Self {
+        let interval = Self::create_interval(interval);
+        TickHelper { interval }
+    }
 
-    // Calculate the difference between now and the next hour
-    let difference = now - epoch;
+    fn create_interval(interval: Duration) -> Interval {
+        let now = OffsetDateTime::now_utc();
+        debug!("Now: {:?}", now);
 
-    // Calculate the difference between now and the next tick
-    let tick_difference = Duration::nanoseconds((now.unix_timestamp_nanos() % interval.whole_nanoseconds()) as i64);
+        let epoch = OffsetDateTime::UNIX_EPOCH;
 
-    // Calculate the next tick
-    let next_tick = difference - tick_difference + interval;
-    println!("Next Tick for new interval: {:?}", epoch + next_tick);
+        // Calculate the difference between now and the next hour
+        let difference = now - epoch;
 
-    // Calculate start time for interval
-    let time_till_start = now - (epoch + next_tick);
-    println!("Time till start: {:?}", time_till_start);
+        // Calculate the difference between now and the next tick
+        let tick_difference = Duration::nanoseconds((now.unix_timestamp_nanos() % interval.whole_nanoseconds()) as i64);
 
-    let start = Instant::now() + StdDuration::from_nanos(-time_till_start.whole_nanoseconds() as u64);
-    println!("Start: {:?}", start);
-    let mut interval = interval_at(start, StdDuration::from_nanos(interval.whole_nanoseconds() as u64));
-    interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
-    println!("Interval created with duration: {:?}", interval);
-    interval
+        // Calculate the next tick
+        let next_tick = difference - tick_difference + interval;
+        debug!("Next Tick for new interval: {:?}", epoch + next_tick);
+
+        // Calculate start time for interval
+        let time_till_start = now - (epoch + next_tick);
+        let start = Instant::now() + StdDuration::from_nanos(-time_till_start.whole_nanoseconds() as u64);
+        let mut interval = interval_at(start, StdDuration::from_nanos(interval.whole_nanoseconds() as u64));
+        interval.set_missed_tick_behavior(MissedTickBehavior::Burst);
+        interval
+    }
+
+    pub async fn tick(&mut self) -> OffsetDateTime {
+        self.interval.tick().await;
+        OffsetDateTime::now_utc()
+            .replace_nanosecond(0)
+            .expect("Failed to replace nanosecond")
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::logging;
+    use test_log::test;
+    use tracing::info;
 
-    #[tokio::test]
-    #[ignore]
+    #[test(tokio::test)]
     async fn test_create_interval() {
-        logging::init_test_tracing();
-        let mut interval = create_interval(Duration::seconds(5));
+        let mut interval = TickHelper::new(Duration::seconds(60));
 
-        interval.tick().await;
-        println!("Timestamp: {:?}", OffsetDateTime::now_utc());
+        // I get the instant of the tick
+        loop {
+            let tick = interval.tick().await;
+            info!("Tick: {:?}", tick);
+        }
     }
 }
