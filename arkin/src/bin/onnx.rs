1a779ca152745c83b77ad3c10f89ee3ebda0bf4e
Dorus Janssens <dorus.janssens@gmail.com>
2025-03-07 17:12:13 +0800

    WIP for ONNX AI support

---
+++ arkin/src/bin/onnx.rs
@@ -0,0 +1,38 @@
+use anyhow::Result;
+use ndarray::Array;
+use ort::session::{builder::GraphOptimizationLevel, Session};
+
+const SEQ_LEN: usize = 16;
+const INPUTS_LEN: usize = 89;
+
+fn main() -> Result<()> {
+    run_ort()?;
+    // run_candle()?;
+
+    Ok(())
+}
+
+fn run_ort() -> Result<()> {
+    // Build the ORT session
+    let model = Session::builder()?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
+        .with_intra_threads(4)?
+        .commit_from_file("../arkin-research/models/CNN-LSTM_trained.onnx")?;
+
+    // Create a 3D input tensor with shape [1, SEQ_LEN, INPUTS_LEN] filled with 1.0
+    let input_array = Array::from_elem((1, SEQ_LEN, INPUTS_LEN), 1.0f32);
+
+    // Run the model with the correct input name "input"
+    let timer = std::time::Instant::now();
+    let mut counter = 0;
+    for _ in 0..10000 {
+        let input = ort::value::Tensor::from_array(input_array.clone().into_dyn())?;
+        let outputs = model.run(ort::inputs!["input" => input].unwrap())?;
+        let predictions = outputs["output"].try_extract_tensor::<f32>()?;
+        println!("ORT Predictions: {:?}", predictions.as_slice());
+        counter += 1;
+    }
+    println!("ORT time: {:?}", timer.elapsed() / counter);
+
+    Ok(())
+}
